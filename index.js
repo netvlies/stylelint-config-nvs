da73e3a9be402775431c59d1db4e3cc84f16ec24
Sjors Spoorendonk <sjors@netvlies.nl>
2017-07-24 12:52:02 +0200

    Removed deprecated block-no-single-line
    https://github.com/stylelint/stylelint/issues/2123

--- index.js
+++ index.js
@@ -158,7 +158,6 @@ module.exports = {
 		"block-closing-brace-newline-before": "always-multi-line",
 		"block-closing-brace-space-after": null,
 		"block-closing-brace-space-before": "always-single-line",
-		"block-no-single-line": null,
 		"block-opening-brace-newline-after": "always-multi-line",
 		"block-opening-brace-newline-before": null,
 		"block-opening-brace-space-after": "always-single-line",
